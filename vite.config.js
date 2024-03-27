a4c20a344c6b029daad444a2c6d7f789dea19a26
jpgm1848 <154918928+jpgm1848@users.noreply.github.com>
2024-03-27 16:41:35 -0400

    created authenticate function/configured vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+const serverPort = 3001;
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": `https://localhost:${serverPort}`,
+    },
+  },
+});
