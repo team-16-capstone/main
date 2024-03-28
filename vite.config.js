2d37c235dd5a90573ad148f42eda0372c0f41d26
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-03-28 14:35:23 -0400

    updated git ignore and vite

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import mkcert from 'vite-plugin-mkcert';
 
 const serverPort = 3001;
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   server: {
     proxy: {
-      "/api": `https://localhost:${serverPort}`,
+      '/api': `https://localhost:${serverPort}`,
     },
+    https: true,
   },
 });
