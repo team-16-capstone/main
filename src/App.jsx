17756adfbea1853e8e5ca8b7f16e89ccc81da3e8
jpgm1848 <154918928+jpgm1848@users.noreply.github.com>
2024-03-29 11:27:11 -0400

    Added registering functionality to front end

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,31 @@
-
-import './App.css';
-import Login from './components/Login';
-import Register from './components/Register';
-import Account from './components/Account';
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
-import MeatYourMatch from './components/MeatYourMatch';
-import NewExperience from './components/NewExperience';
-import Butchers from './components/Butchers';
-import Community from './components/Community';
-import StripeTest from './components/StripeTest';
-
-
+import "./App.css";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Account from "./components/Account";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import MeatYourMatch from "./components/MeatYourMatch";
+import NewExperience from "./components/NewExperience";
+import Butchers from "./components/Butchers";
+import Community from "./components/Community";
+import StripeTest from "./components/StripeTest";
 
 function App() {
   const [auth, setAuth] = useState([]);
 
-
   const devUser = {
     id: "7",
     token: "10489571n45gjqckjnv",
-    email: "devuser@email.com"
-
+    email: "devuser@email.com",
   };
 
   useEffect(() => {
     // console.log(auth);
     if (auth.id) {
-      console.log('load user account');
-    }
-    else {
-      console.log('clear user account');
-
+      console.log("load user account");
+    } else {
+      console.log("clear user account");
     }
     // client.Meat.findMany().then((response) =>
     //   console.log("this is the response", response)
@@ -41,11 +34,10 @@ function App() {
 
   useEffect(() => {
     const attemptLoginWithToken = async () => {
-      const response = await fetch('https://pocketbutcher.com/api/users/me', {
+      const response = await fetch("https://pocketbutcher.com/api/users/me", {
         headers: {
-          Authorization: `Bearer ${token}`
-
-        }
+          Authorization: `Bearer ${token}`,
+        },
       });
       const json = await response.json();
       if (response.ok) {
@@ -56,27 +48,25 @@ function App() {
     if (token) {
       attemptLoginWithToken();
     } else {
-
     }
   }, []);
 
-
   const login = async (credentials) => {
-    let response = await fetch('https://pocketbutcher.com/api/users/login', {
-      method: 'POST',
+    let response = await fetch("https://pocketbutcher.com/api/users/login", {
+      method: "POST",
       body: JSON.stringify(credentials),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     });
     let json = await response.json();
     if (response.ok) {
       const token = json.token;
-      window.localStorage.setItem('token', token);
-      response = await fetch('https://pocketbutcher.com/api/users/me', {
+      window.localStorage.setItem("token", token);
+      response = await fetch("https://pocketbutcher.com/api/users/me", {
         headers: {
-          Authorization: `Bearer ${token}`
-        }
+          Authorization: `Bearer ${token}`,
+        },
       });
       json = await response.json();
       if (response.ok) {
@@ -84,51 +74,46 @@ function App() {
       } else {
         setAuth(devUser);
       }
-    }
-    else {
+    } else {
       console.log(json);
-
     }
   };
 
   const register = async (credentials) => {
-    let response = await fetch('https://pocketbutcher.com/api/users/register', {
-      method: 'POST',
+    let response = await fetch("http://localhost:3001/api/users", {
+      method: "POST",
       body: JSON.stringify(credentials),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     });
     let json = await response.json();
     if (response.ok) {
       const token = json.token;
-      window.localStorage.setItem('token', token);
-      response = await fetch('https://pocketbutcher.com/api/users/me', {
+      window.localStorage.setItem("token", token);
+      response = await fetch("http://localhost:3001/api/users", {
         headers: {
-          Authorization: `Bearer ${token}`
-        }
+          Authorization: `Bearer ${token}`,
+        },
       });
       json = await response.json();
       if (response.ok) {
         setAuth(json);
       }
-    }
-    else {
+    } else {
       console.log(json);
     }
   };
 
-//   const logout = ()=> {
-//     window.localStorage.removeItem('token');
-//     setAuth({});
-//   };
+  //   const logout = ()=> {
+  //     window.localStorage.removeItem('token');
+  //     setAuth({});
+  //   };
 
   return (
     <>
       <div>
-
         <h1 id="logo">Pocket Butcher</h1>
-
       </div>
       {/* <nav>
           {
@@ -141,44 +126,25 @@ function App() {
           </div>
       </nav> */}
 
-
-      {
-        auth.id ? (
-          <button onClick={logout}>LOGOUT</button>
-        ) :
-          (
-            <>
-
-
-              <Routes>
-                <Route
-                  path='/' element={<Login login={login} />} >
-                </Route>
-                <Route
-                  path='/register' element={<Register register={register} />} >
-                </Route>
-                <Route
-                  path='/account' element={<Account />} >
-                </Route>
-                <Route
-                  path='/meat-your-match' element={<MeatYourMatch />} >
-                </Route>
-                <Route
-                  path='/new-experience' element={<NewExperience />} >
-                </Route>
-                <Route
-                  path='/butchers' element={<Butchers />} >
-                </Route>
-                <Route
-                  path='/community' element={<Community />} >
-                </Route>
-                <Route
-                  path='/stripetest' element={<StripeTest />} >
-                </Route>
-              </Routes>
-            </>
-          )
-      }
+      {auth.id ? (
+        <button onClick={logout}>LOGOUT</button>
+      ) : (
+        <>
+          <Routes>
+            <Route path="/" element={<Login login={login} />}></Route>
+            <Route
+              path="/register"
+              element={<Register register={register} />}
+            ></Route>
+            <Route path="/account" element={<Account />}></Route>
+            <Route path="/meat-your-match" element={<MeatYourMatch />}></Route>
+            <Route path="/new-experience" element={<NewExperience />}></Route>
+            <Route path="/butchers" element={<Butchers />}></Route>
+            <Route path="/community" element={<Community />}></Route>
+            <Route path="/stripetest" element={<StripeTest />}></Route>
+          </Routes>
+        </>
+      )}
     </>
   );
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,45 +1,46 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import {Link} from 'react-router-dom';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const Login = ({ login })=> {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const Login = ({ login }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const navigate = useNavigate();
-  
-  const submit = async(ev)=> {
+
+  const submit = async (ev) => {
     ev.preventDefault();
     const credentials = {
       email,
-      password
+      password,
     };
     await login(credentials);
-
-  }
+  };
 
   return (
     <>
-    <div id="login-container">
-    <form onSubmit={ submit }>
-      <input
-        placeholder='email'
-        value={ email }
-        onChange={ ev => setEmail(ev.target.value )}
-      />
-      <br/>
-      <input
-        placeholder='password'
-        value={ password }
-        type="password"
-        onChange={ ev => setPassword(ev.target.value )}
-      />
-      <br/>
-      {/* <button onClick={() => (submit)}>LOGIN</button> */}
-      <button onClick={() => navigate('/account')}>LOGIN</button>
-    </form>
-    <Link className="link" to='/register'>Become a Member</Link>
-    </div>
+      <div id="login-container">
+        <form onSubmit={submit}>
+          <input
+            placeholder="email"
+            value={email}
+            onChange={(ev) => setEmail(ev.target.value)}
+          />
+          <br />
+          <input
+            placeholder="password"
+            value={password}
+            type="password"
+            onChange={(ev) => setPassword(ev.target.value)}
+          />
+          <br />
+          {/* <button onClick={() => submit}>LOGIN</button> */}
+          <button onClick={() => navigate("/account")}>LOGIN</button>
+        </form>
+        <Link className="link" to="/register">
+          Become a Member
+        </Link>
+      </div>
     </>
   );
 };
