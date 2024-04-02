13feae74a44c0e792eb28e78797c94fc960bbdd9
jpgm1848 <154918928+jpgm1848@users.noreply.github.com>
2024-04-02 15:04:15 -0400

    Correctly authenticates user token

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,14 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
-const useAuth = () => {
+const checkIfAuthenticated = async () => {
   const token = localStorage.getItem('token');
-  const isLoggedIn = !!token;
-  return isLoggedIn;
+  if (token) {
+    try {
+      const response = await fetch('http://localhost:3001/api/verifyToken', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.ok) {
+        return true;
+      } else {
+        localStorage.removeItem('token');
+        return false;
+      }
+    } catch (error) {
+      console.error('Error verifying token:', error);
+      localStorage.removeItem('token');
+      return false;
+    }
+  } else {
+    localStorage.removeItem('token');
+    return false;
+  }
 };
 
 const ProtectedRoute = () => {
-  const isLoggedIn = useAuth();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await checkIfAuthenticated();
+      setIsLoggedIn(result);
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return isLoggedIn ? <Outlet /> : <Navigate to='/' />;
 };
 
