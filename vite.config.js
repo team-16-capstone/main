887f9c9d72df8313fe4696547a38ebe9f998e162
jpgm1848 <154918928+jpgm1848@users.noreply.github.com>
2024-04-16 12:05:12 -0400

    changed script path

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import mkcert from 'vite-plugin-mkcert';
+import process from 'process';
 
-const serverPort = 3001;
+const serverPort = process.env.PORT || 3001;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), mkcert()],
   server: {
     cors: true,
-    //   proxy: {
-    //     '/api': `https://localhost:${serverPort}`,
-    //   },
-    //   https: true,
+    proxy: {
+      '/api': `https://localhost:${serverPort}`,
+    },
+    https: true,
   },
 });
