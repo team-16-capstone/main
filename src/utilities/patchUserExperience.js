c73c0e8093a6ab54eb5bfc15b56864afd2cc54c0
jpgm1848 <154918928+jpgm1848@users.noreply.github.com>
2024-04-17 11:49:35 -0400

    Corrected patchUserExperience

--- src/utilities/patchUserExperience.js
+++ src/utilities/patchUserExperience.js
@@ -1,7 +1,7 @@
 const patchUserExperience = async (experienceId, token, experience) => {
   try {
     const response = await fetch(
-      `https://pocket-butcher-backend.onrender.com/api/${experienceId}`,
+      `https://pocket-butcher-backend.onrender.com/api/experiences/${experienceId}`,
       {
         method: 'PATCH',
         headers: {
