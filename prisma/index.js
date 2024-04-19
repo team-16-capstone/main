cb69e5e5c377f43a0d8f1e529fb8f2c54645451b
Julie Hildabrand <jhildabrand257@gmail.com>
2024-04-19 12:16:42 -0400

    morning tweaks

--- prisma/index.js
+++ prisma/index.js
@@ -674,7 +674,7 @@ app.post('/api/verifytoken', (req, res) => {
   });
 });
 
-const port = process.env.PORT || 3001;
+const port = 3001;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}.`);
